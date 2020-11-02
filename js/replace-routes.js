4160950c3b5bf4e1a9ec4738b2eff2baf960dd97
Daywison Silva <daywisonsilvasou@gmail.com>
2020-11-02 17:16:10 -0300

    finalizando a landing page

--- js/replace-routes.js
+++ js/replace-routes.js
@@ -6,24 +6,28 @@ document.querySelector('.pacote2').addEventListener('click', () => {
     let rotas = document.querySelectorAll('.js-rota');
     let oldPrice = document.querySelector('.old-price-pacote1');
     let newPrice = document.querySelector('.new-price-pacote1')
+    let titleCard = document.querySelector('.text-pacote');
     rotas = Array.from(rotas);
     rotas[0].textContent = 'Cachoeira do Arari';
     rotas[1].textContent = 'Salvaterra';
     rotas[2].textContent = 'Soure';
     rotas[3].textContent = 'Ponta de Pedras';
     oldPrice.textContent = 'De R$2.569,00';
-    newPrice.textContent = 'Por R$2.200,00'
+    newPrice.textContent = 'Por R$2.200,00';
+    titleCard.textContent = 'Pacote 2';
 }) 
 
 document.querySelector('.pacote1').addEventListener('click', () => {
     let rotas = document.querySelectorAll('.js-rota');
     let oldPrice = document.querySelector('.old-price-pacote1');
-    let newPrice = document.querySelector('.new-price-pacote1')
+    let newPrice = document.querySelector('.new-price-pacote1');
+    let titleCard = document.querySelector('.text-pacote');
     rotas = Array.from(rotas);
     rotas[0].textContent = 'Combú';
     rotas[1].textContent = 'Sirituba';
     rotas[2].textContent = 'Cotijuba';
     rotas[3].textContent = 'Mosqueiro';
     oldPrice.textContent = 'De R$1.857,99';
-    newPrice.textContent = 'Por R$1.530,00'
+    newPrice.textContent = 'Por R$1.530,00';
+    titleCard.textContent = 'Pacote 1';
 }) 
